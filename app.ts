9bbdc4e7e8b21c86502ba467f217c68bda298225
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-28 01:35:19 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,19 @@
+//task 1
+// installation complete
+
+// type car = {
+//     manufacture: string
+//     model: string
+//     [key: string]: any;
+// }
+
+// function creatCar(manufacture: string, model: string, optional: Record<string, any>): car {
+//     return{
+//         manufacture,
+//         model,
+//         ...optional
+//     }
+// }
+
+// const mycar: car = creatCar("toyota" , "corolla", {colour: "silver" , year: "2024"})
+// console.log(mycar)
